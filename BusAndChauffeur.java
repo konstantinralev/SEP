e75f3e296ed62b1ca38b97dcc4819ce2a5a5a7ce
konstantinralev <konstantin.ralev@gmail.com>
2016-12-01 14:31:39 +0100

    ShityClasses

---
+++ BusAndChauffeur.java
@@ -0,0 +1,24 @@
+package sep;
+
+import java.util.ArrayList;
+public class BusAndChauffeur
+{ 
+   public BusAndChauffeur(){
+      
+   }
+   
+   public void setExtraServices (String services){
+      
+   }
+   
+   public ArrayList<String> getExtraServices{
+    ArrayList<String> extra = new ArrayList<>();
+
+   }
+   
+   public String toString(){
+      return null;
+      
+   }
+
+}
