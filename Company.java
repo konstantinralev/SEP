309b65e8a9e6d21d25238e47f10a91006b33e2d5
konstantinralev <konstantin.ralev@gmail.com>
2016-12-02 17:37:59 +0100

    Company Class

---
+++ Company.java
@@ -0,0 +1,38 @@
+package sep;
+
+public class Company
+{
+   public Bus[] suggestBusses(){
+      
+   }
+   public void addBus(Bus bus){
+      
+   }
+   public void removeBus(int id){
+      
+   }
+   public int getNumberOfBusses(){
+      
+   }
+   public int getNumberOfReservations(){
+      
+   }
+   public void applyDiscount(){
+      
+   }
+   public Chauffeur[] suggestChauffeurs(){
+      
+   }
+   public void addChauffeur(Chauffeur chauffeur){
+      
+   }
+   public void removeChauffeur(Chauffeur chauffeur){
+      
+   }
+   public int getNumbersOfChauffeurs(){
+      
+   }
+   public int getNumbersOfServices(){
+      
+   }
+}
