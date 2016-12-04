0585cdbc4cb3ca27a82655915b912e5dea6d6086
saralopez7 <slopezalaguero@gmail.com>
2016-12-04 22:54:12 +0100

    Route Updated

--- Route.java
+++ Route.java
@@ -4,27 +4,49 @@ public class Route
    private String placeOfDeparture;
    private String placeOfArrival;
    
+   /**
+    * sets the placeOfArrival and the placeOfDeparture of each route
+    * @param placeOfArrival
+    * @param placeOfDeparture
+    */
    public Route(String placeOfArrival,String placeOfDeparture){
       this.placeOfArrival = placeOfArrival;
       this.placeOfDeparture = placeOfDeparture;
    }
    
+   
+   /**
+    * @param placeofArrival
+    */
    public void setPlaceOfArrival(String placeofArrival){
       this.placeOfArrival = placeofArrival;
    }
    
-   public void setPlaceOfDeparture(String placeofDeparture){
-      this.placeOfDeparture = placeofDeparture;
-   }
-   
+   /**
+    * @return placeOfArrival
+    */
    public String getPlaceOfArrival(){
       return placeOfArrival;
    }
    
+   /**
+    * @param placeofDeparture
+    */
+   public void setPlaceOfDeparture(String placeofDeparture){
+      this.placeOfDeparture = placeofDeparture;
+   }
+   
+   /**
+    * @return placeOfDeparture
+    */
    public String getPlaceOfDeparture(){
       return placeOfDeparture;
    }
    
+   
+   /**
+    * @return information about each route
+    */
    public String toString(){
       return "From: " + placeOfDeparture + "\nTo: " + placeOfArrival;
    }
