a7f60b50e613287fa1b0ed30a6a066fde258f920
Deyana <deyana.atanasova04@gmail.com>
2016-12-04 21:44:37 +0100

    Bus Perfected

--- Bus.java
+++ Bus.java
@@ -6,20 +6,19 @@ public class Bus
    private String type;
    private Reservation reservation;
    
+   /**
+    * sets the type, capacity and plate number of each bus
+    * @param type
+    * @param capacity
+    * @param id
+    */
    public Bus(String type, int capacity, int id){
       this.type= type;
       this.capacity = capacity;
       this.id = id;
    }
 
-   /**
-    * @return the capacity
-    */
-   public int getCapacity()
-   {
-      return capacity;
-   }
-
+   
    /**
     * @param capacity the capacity to set
     */
@@ -27,6 +26,25 @@ public class Bus
    {
       this.capacity = capacity;
    }
+
+   
+   /**
+    * @return the capacity
+    */
+   public int getCapacity()
+   {
+      return capacity;
+   }
+   
+   
+   /**
+    * @param id the id to set
+    */
+   public void setId(int id)
+   {
+      this.id = id;
+   }
+
 
    /**
     * @return the id
@@ -36,21 +54,6 @@ public class Bus
       return id;
    }
 
-   /**
-    * @param id the id to set
-    */
-   public void setId(int id)
-   {
-      this.id = id;
-   }
-
-   /**
-    * @return the type
-    */
-   public String getType()
-   {
-      return type;
-   }
 
    /**
     * @param type the type to set
@@ -60,13 +63,30 @@ public class Bus
       this.type = type;
    }
 
+   
+   /**
+    * @return the type
+    */
+   public String getType()
+   {
+      return type;
+   }
+
+   
+   /**
+    * 
+    * @param dateInterval
+    * @return if the bus is available
+    */
+   public boolean isAvailable(DateInterval dateInterval){
+      //put code checking for availability here
+   }
+   
+   
    public int getFreeSeats(){
       return capacity - reservation.getNumberOfPassengers();
    }
    
-   public boolean isAvailable(DateInterval dateInterval){
-      //put code checking for availability here
-   }
    
    public String toString(){
       return "Capacity: " + capacity + "\nPlate Number: " + id + "\nType: " + type;
