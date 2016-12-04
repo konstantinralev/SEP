5e81929213b8295d921feeda1e3540237d4b5393
Animosa04 <deyana.atanasova04@gmail.com>
2016-12-04 16:34:57 +0100

    Add files via upload
    ChauffeurList Class

---
+++ ChauffeurList.java
@@ -0,0 +1,37 @@
+import java.util.ArrayList;
+
+public class ChauffeurList
+{
+   private ArrayList<Chauffeur> chauffeurs;   
+   
+   public ChauffeurList(){
+      chauffeurs = new ArrayList<Chauffeur>();
+   }
+   /**
+    * look for a chauffeur by id
+    * @param id
+    * @return chauffeur
+    */
+   public Chauffeur getChauffeur(int id){
+      for(int i = 0; i < chauffeurs.size(); i++){
+         if (chauffeurs.get(i).getId() == id)
+         {
+            return chauffeurs.get(i);
+         }
+      }
+      return null;
+   }
+   
+   
+   /**
+    * @return a string with all of the chauffeurs
+    */
+   public String toString(){
+      String text = "Chauffeurs:\n";
+      for (int i = 0; i < chauffeurs.size(); i++)
+      {
+        text += chauffeurs.get(i).getName() + "\n"; 
+      }
+      return text;
+   }
+}
