a79d2b9a0ebc81305d6fbfd67b0ae154024e156e
Animosa04 <deyana.atanasova04@gmail.com>
2016-12-04 23:05:08 +0100

    DateInterval Perfected

--- DateInterval.java
+++ DateInterval.java
@@ -5,42 +5,53 @@ public class DateInterval
    private Date departureDate;
    private Date arrivalDate;
    
+   
+   /**
+    * sets the departure date and the arrival date of the date interval
+    * @param departureDate
+    * @param arrivalDate
+    */
    public DateInterval(Date departureDate, Date arrivalDate){
       this.departureDate = departureDate;
       this.arrivalDate = arrivalDate;
    }
    
-   /**
-    * @return the departureDate
-    */
-   public Date getDepartureDate()
-   {
-      return departureDate;
-   }
 
    /**
-    * @param departureDate the departureDate to set
+    * @param departureDate to set the departureDate
     */
    public void setDepartureDate(Date departureDate)
    {
       this.departureDate = departureDate;
    }
 
+   
    /**
-    * @return the arrivalDate
+    * @return the departureDate
     */
-   public Date getArrivalDate()
+   public Date getDepartureDate()
    {
-      return arrivalDate;
+	   return departureDate;
    }
 
+   
    /**
-    * @param arrivalDate the arrivalDate to set
+    * @param arrivalDate to set the arrivalDate
     */
    public void setArrivalDate(Date arrivalDate)
    {
       this.arrivalDate = arrivalDate;
    }
+   
+
+   /**
+    * @return the arrivalDate
+    */
+   public Date getArrivalDate()
+   {
+	   return arrivalDate;
+   }
+   
    
    /**
     * 
@@ -51,5 +62,12 @@ public class DateInterval
       return (this.departureDate.before(other.departureDate)&&this.arrivalDate.after(other.departureDate)) ||
             (other.departureDate.before(this.departureDate)&&other.arrivalDate.after(this.departureDate));
       }
+   
+   
+   /**
+    * @return a string with the departure and arrival date
+    */
+   public String toString(){
+	   return "Departure date: " + departureDate + "\nArrival date: " + arrivalDate;
    }
-
+}
