8b994346f3a8c5de08e8ef562cce52a13fd6f940
saralopez7 <slopezalaguero@gmail.com>
2016-12-05 00:09:42 +0100

    Company updated

--- Company.java
+++ Company.java
@@ -1,38 +1,44 @@
-package sep;
+import java.util.ArrayList;
+
 
 public class Company
 {
-   public Bus[] suggestBusses(){
-      
+   private CustomerList customers;
+   private Reservation reservation;
+   
+   /**
+    * 
+    * @return a list with the suggested buses depending on its availability
+    */
+   public ArrayList<Bus> suggestBuses(){
+      ArrayList<Bus> suggestedBuses = new ArrayList<Bus>();
+      for(int i = 0; i< suggestedBuses.size();i++){
+         
+      }
    }
-   public void addBus(Bus bus){
-      
+   /** 
+    * applies discount if the customer is regular
+    */
+   public void applyDiscount(String customerName){
+      if(customers.isRegular(customerName)){
+         double discount = (reservation.getPrice() - (reservation.getPrice()*0.2));
+         reservation.setPrice(discount); 
+      }
    }
-   public void removeBus(int id){
-      
-   }
-   public int getNumberOfBusses(){
-      
-   }
-   public int getNumberOfReservations(){
-      
-   }
-   public void applyDiscount(){
-      
-   }
+   /**
+    * 
+    * @return a list with the suggested Chauffeurs depending on the dates and the Chauffeurs' wishes.
+    */
    public Chauffeur[] suggestChauffeurs(){
       
    }
-   public void addChauffeur(Chauffeur chauffeur){
+   /**
+    * 
+    * @return a lis with the suggested Services depending on the dates. 
+    */
+   public Service[] suggestService(){
       
    }
-   public void removeChauffeur(Chauffeur chauffeur){
-      
-   }
-   public int getNumbersOfChauffeurs(){
-      
-   }
-   public int getNumbersOfServices(){
-      
-   }
+   
+ 
 }
