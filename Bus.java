babb1f094421de007f8297d5ec4b339c95835227
Deyana <deyana.atanasova04@gmail.com>
2016-12-04 14:20:52 +0100

    Bus Updated

--- Bus.java
+++ Bus.java
@@ -12,50 +12,95 @@ public class Bus
       this.id = id;
    }
 
+   /**
+    * @return the capacity
+    */
    public int getCapacity()
    {
       return capacity;
    }
 
-   public int getId()
-   {
-      return id;
-   }
 
-   public String getType()
-   {
-      return type;
-   }
 
+   /**
+    * @param capacity the capacity to set
+    */
    public void setCapacity(int capacity)
    {
       this.capacity = capacity;
    }
 
+
+
+   /**
+    * @return the id
+    */
+   public int getId()
+   {
+      return id;
+   }
+
+
+
+   /**
+    * @param id the id to set
+    */
    public void setId(int id)
    {
       this.id = id;
    }
 
+
+
+   /**
+    * @return the type
+    */
+   public String getType()
+   {
+      return type;
+   }
+
+
+
+   /**
+    * @param type the type to set
+    */
    public void setType(String type)
    {
       this.type = type;
    }
+
+
+
+   /**
+    * @return the reservation
+    */
+   public Reservation getReservation()
+   {
+      return reservation;
+   }
+
+
+
+   /**
+    * @param reservation the reservation to set
+    */
+   public void setReservation(Reservation reservation)
+   {
+      this.reservation = reservation;
+   }
+
    
    public int getFreeSeats(){
       return capacity - reservation.getNumberOfPassengers();
    }
    
    public boolean isAvailable(DateInterval dateInterval){
-      if(reservation.getDateInterval()<= dateInterval){
-        return true; 
-      }else{
-      return false;
-      }
+      //put code checking for availability here
    }
    
    public String toString(){
-      return "Capacity: " + capacity + "\n" + "Id: " + id + "\n" + "Type: " + type;
+      return "Capacity: " + capacity + "\nPlate Number: " + id + "\nType: " + type;
    }
 
 }
